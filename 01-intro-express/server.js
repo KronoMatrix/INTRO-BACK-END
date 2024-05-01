128cff57fd827d7d46813813a9dc572f5d84ae02
Erick Jimenez Rubi <141981230+KronoMatrix@users.noreply.github.com>
2024-04-30 21:54:29 -0600

    Add files via upload

---
+++ 01-intro-express/server.js
@@ -0,0 +1,24 @@
+// #1 Llamar a la biblioteca de express
+const express = require('express'); // Common.JS
+const petRoutes = require ('./api/v1/pets');
+const cakeRoutes = require ('./api/v1/');
+
+// #2 Crear una instancia de express
+const app = express();
+
+app.use(express.urlencoded({ extended: true })); // urlencoded me permite recibir información más rica en el body
+app.use(express.json()); // json me permite recibir información en formato json
+
+// #3 Definir las rutas
+app.get('/', (req, res) => {
+    res.send('Hola Mundo');
+});
+
+// #4 Levantar el servidor en un puerto, por ejemplo el 3000
+app.listen(3000, () => {
+    console.log('Servidor corriendo en http://localhost:3000 🚀');
+});
+
+// #5 Puedo importar rutas de un archivo externo
+app.use(petRoutes);
+app.use(cakeRoutes);
